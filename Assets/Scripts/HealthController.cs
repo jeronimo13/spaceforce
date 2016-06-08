c249022947bf3facbec282e0613b5b52b91bd6bb
Herman Havrish <gavrishgerman@yandex.ru>
2016-06-08 18:51:28 +0200

    Overlap stop and hit visual

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -8,7 +8,14 @@ public class HealthController : MonoBehaviour {
 	public GameObject star;
 	public float starGetPercentage = 0.7f;
 	public float explosionForce = 1;
+	public float explosionRadius = 1;
+	public GameObject mainRendererHolder ;
 
+	private Renderer mainRenderer;
+
+	void Start(){
+		mainRenderer = mainRendererHolder.GetComponent<Renderer> ();
+	}
 
 	public void Explode(){
 		Instantiate (explosion, transform.position, transform.rotation);
@@ -21,19 +28,31 @@ public class HealthController : MonoBehaviour {
 		if (!isAlive ()) {
 			if (star != null && (Random.value < starGetPercentage)) {
 				for (int i = 0; i < Random.Range (2, 5); i++) {
-					Vector3 pos = new Vector3 (inst.transform.position.x + Random.value - 0.5f, inst.transform.position.y , inst.transform.position.z + Random.value);
-					GameObject starInst = Instantiate (star, pos, Quaternion.Euler(0,0,0)) as GameObject;
-					starInst.GetComponent<Rigidbody> ().AddExplosionForce (explosionForce, inst.transform.position, 1, 0, ForceMode.Impulse);
-					StartCoroutine (ChangeAngularDrag (starInst));
+					Vector3 pos = new Vector3 (inst.transform.position.x + Random.value - 0.5f, inst.transform.position.y, inst.transform.position.z + Random.value);
+					GameObject starInst = Instantiate (star, pos, Quaternion.Euler (0, 0, 0)) as GameObject;
+					Collider[] colliders = Physics.OverlapSphere (inst.transform.position, explosionRadius);
+					foreach (Collider c in colliders) {
+						if (c.CompareTag ("Star")) {
+							c.GetComponent<Rigidbody> ().AddExplosionForce (explosionForce, inst.transform.position, 1, 0, ForceMode.Impulse);
+						}
+					}
 				}
 			}
 			Explode ();
+		} else {
+			StartCoroutine (collideFlash());
 		}
 	}
 
-	IEnumerator ChangeAngularDrag(GameObject obj){
-		yield return new WaitForSeconds (0.5f);
-		obj.GetComponent<Rigidbody> ().drag = 0;
+
+	IEnumerator collideFlash() {
+		Material m = this.mainRenderer.material;
+		Color32 c = this.mainRenderer.material.color;
+		this.mainRenderer.material = null;
+		this.mainRenderer.material.color = Color.red;
+		yield return new WaitForSeconds(0.1f);
+		this.mainRenderer.material = m;
+		this.mainRenderer.material.color = c;            
 	}
 
 	private void updateHealth (int value)
---
+++ Assets/StopOverlap.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopOverlap : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.CompareTag ("Star")) {
+			gameObject.GetComponent<Rigidbody> ().AddForce (new Vector3 (Random.value,0, Random.value),ForceMode.Impulse);
+		}
+	}
+}
